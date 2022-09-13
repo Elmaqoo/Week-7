ce5a5b91f193f2e59d4f371bcbbb249d468196fc
Elmaqoo <110962513+Elmaqoo@users.noreply.github.com>
2022-09-13 11:32:58 +1000

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello World! Local Component" />
+    </div>
+  </header>
+
+
+</template>
+
+<style>
+</style>
